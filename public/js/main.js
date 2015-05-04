221380065a3a752700459df0a746eccb2d02777e
Samuel Vogel <samuel.vogel@viison.com>
2015-05-04 14:27:30 +0200

    Add input range value indicators

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,10 @@
+$(function ($) {
 
+	// Show input range value in corresponding output tag
+	$('input[type=range]').on('input', function (event) {
+		$('output[name=' + this.id + '-indicator]').text(this.valueAsNumber);
+	});
+	// Initialize all indicators
+	$('input[type=range]').trigger('input');
+
+})
